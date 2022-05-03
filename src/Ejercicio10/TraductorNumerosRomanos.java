b7d66bce67a28cd78c0d31a1b5cbb6ce9854ab0b
Gabriel-2A <gaa.021205@gmail.com>
2022-05-02 22:02:26 -0500

    Avance ejercicio 10

--- src/Ejercicio10/TraductorNumerosRomanos.java
+++ src/Ejercicio10/TraductorNumerosRomanos.java
@@ -2,5 +2,24 @@ package Ejercicio10;
 
 public class TraductorNumerosRomanos {
 
+    public static void main(String[] args) {
+
+    }
+
+
+    class ConvertirNumeroRomanos{
+
+        private String milesimas;
+        private String centesimas;
+        private String decimales;
+        private String unidades;
+        private String numeroRomano;
+        private int numero;
+
+
+
+
+    }
+
 
 }
